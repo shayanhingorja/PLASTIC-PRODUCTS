0afe8214535466be5059f6cd6d1a1a63f8d182fe
shayanhingorja <145749534+shayanhingorja@users.noreply.github.com>
2024-12-04 21:06:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile Menu Toggle
+const mobileMenu = document.getElementById("mobile-menu");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
